bc6dd4fab0b222c39a994b95cf1b1bb637c2a16b
devcat <jaeheon.dev@gmail.com>
2021-01-05 17:23:32 +0900

    update epsilon greedy

--- DQN/main.py
+++ DQN/main.py
@@ -2,6 +2,10 @@ import gym
 import random
 import numpy as np
 from collections import deque
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 class ReplayBuffer:
     def __init__(self, capacity, batch_size):
@@ -14,21 +18,42 @@ class ReplayBuffer:
 
     def sample(self, batch_size):
         state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state), done
 
-class DQNAgent:
-    def __init__(self, action_size):
-        self.state_size = (84, 84, 4)
-        self.action_size = action_size
-        self.epsilon = 1.
-        self.epsilon_start, self.epsilon_end = 1.0, 0.1
-        self.batch_size = 32
-        self.discount_factor = 0.99
-        self.memory = deque(maxlen=400000)
+    def __len__(self):
+        return len(self.buffer)
 
-    def get_action(self, history):
-        history = np.float32(history / 255.0)
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
+class DQN(nn.Module):
+    def __init__(self):
+        super(DQN, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(3, 16, 8, 4),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 4, 2),
+            nn.ReLU(),
+            nn.Linear(x, 256),
+            nn.Linear(256, env.action_space.n)
+        )
+
+    def foward(self, x):
+        return self.layers(x)
+
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state = torch.FloatTensor(state).unsqueeze(0)
+            q_value = self.foward(state)
+            action = q_value.max(1)[1].data[0]
         else:
-            q_value = self.model.predict(history) # need build_model method
-            return np.argmax(q_value[0])
+            action = random.randrange(env.action_space.n)
+        return action
+
+if __name__ == "__main__":
+    env = gym.make('BreakoutDeterministic-v4')
+    model = DQN()
+    if torch.cuda.is_availabe():
+        model = model.cuda()
+    optimizer = optim.Adam(model.parameters())
+    replay_buffer = ReplayBuffer(1000)
+
+
