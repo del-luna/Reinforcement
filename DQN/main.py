1fff2e0158cbf16f949f04f15eb048ef7f5a1ca2
devcat <jaeheon.dev@gmail.com>
2021-01-05 14:44:17 +0900

    dqn update

---
+++ DQN/main.py
@@ -0,0 +1,34 @@
+import gym
+import random
+import numpy as np
+from collections import deque
+
+class ReplayBuffer:
+    def __init__(self, capacity, batch_size):
+        self.buffer = deque(maxlen=capacity)
+    
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+
+class DQNAgent:
+    def __init__(self, action_size):
+        self.state_size = (84, 84, 4)
+        self.action_size = action_size
+        self.epsilon = 1.
+        self.epsilon_start, self.epsilon_end = 1.0, 0.1
+        self.batch_size = 32
+        self.discount_factor = 0.99
+        self.memory = deque(maxlen=400000)
+
+    def get_action(self, history):
+        history = np.float32(history / 255.0)
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            q_value = self.model.predict(history) # need build_model method
+            return np.argmax(q_value[0])
